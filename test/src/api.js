4ec214d0b16dc6351b646e2b704621352bd102cd
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2022-05-01 16:38:43 +0200

    :test_tube: test: Test with larger arrays.

--- test/src/api.js
+++ test/src/api.js
@@ -4,7 +4,7 @@ import {macro, units, linearScan} from '#module';
 
 const seed = [123, 456];
 
-for (const options of [{}, {length: [333]}]) {
+for (const options of [{}, {length: [333, 1000]}]) {
 	for (const unit of units(options)) {
 		test(macro, {...unit, seed, search: linearScan});
 	}
